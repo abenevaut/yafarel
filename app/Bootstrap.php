7ebd9b4aa185105734632e3bb5f518e72df085cc
Antoine Benevaut <antoine.benevaut@primeum.com>
2023-04-13 16:44:07 +0200

    wip

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -10,28 +10,8 @@ final class Bootstrap extends Bootstrap_Abstract
         Loader::getInstance()->import(PROJECT_PATH . '/vendor/autoload.php');
     }
 
-    public function _initSession(\Yaf\Dispatcher $dispatcher)
-    {
-        ini_set('session.name', 'my-identity');
-    }
-
-    public function _initConfig()
-    {
-
-    }
-
     public function _initPlugin(\Yaf\Dispatcher $dispatcher)
     {
         $dispatcher->registerPlugin(new DisallowFrameEmbeddingPlugin());
-    }
-
-    public function _initRoute(\Yaf\Dispatcher $dispatcher)
-    {
-        //Register your own routing protocol here, by default using a simple route
-    }
-
-    public function _initView(\Yaf\Dispatcher $dispatcher)
-    {
-        //Register your view controller here, for example smarty, firekylin
     }
 }
