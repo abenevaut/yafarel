50420608b2f1d6f563325d1d6f31d0a2d62a0eaf
Antoine Benevaut <me@abenevaut.dev>
2023-04-14 22:20:31 +0200

    chore: Use YAF to registry app & view instances

--- tests/YafUnit.php
+++ tests/YafUnit.php
@@ -10,17 +10,14 @@ use Yaf\View\Simple as ViewSimple;
 
 trait YafUnit
 {
-    public static ?Application $app = null;
-    public static ?ViewSimple $viewEngine = null;
-
     public function getApplication(): Application
     {
-        return self::$app;
+        return Registry::get('application');
     }
 
     public function getView(): ViewSimple
     {
-        return self::$viewEngine;
+        return Registry::get('view');
     }
 
     public function get(string $uri): HttpResponse
@@ -35,17 +32,15 @@ trait YafUnit
 
     protected function createApplication(): self
     {
-        if (!Registry::get('ApplicationInit')) {
-            self::$app = new Application(PROJECT_PATH . '/app.ini');
-            self::$viewEngine = new ViewSimple(PROJECT_PATH . '/app/views');
+        if (!Registry::get('application')) {
+            Registry::set('application', new Application(PROJECT_PATH . '/app.ini'));
+            Registry::set('view', new ViewSimple(PROJECT_PATH . '/app/views'));
 
             $this->getApplication()->bootstrap();
             $this->getApplication()->getDispatcher()->autoRender(false);
             $this->getApplication()->getDispatcher()->flushInstantly(false);
             $this->getApplication()->getDispatcher()->returnResponse(true);
             $this->getApplication()->getDispatcher()->setView($this->getView());
-
-            Registry::set('ApplicationInit', true);
         }
 
         return $this;
